fd12f31012a3f7274bad8fadd7119b3def478ccb
Cedrik Hoffmann <choffmann@progeek.de>
2024-12-19 13:36:40 +0100

    feat: add rofi to i3

--- home-manager/common/i3/default.nix
+++ home-manager/common/i3/default.nix
@@ -1,5 +1,9 @@
 {pkgs, ...}:
 {
+  imports = [ 
+    ../rofi
+  ];
+
   home.file.".config/i3" = {
     source = ./config;
     recursive = true;
---
+++ home-manager/common/rofi/default.nix
@@ -0,0 +1,30 @@
+{ pkgs, ...}:
+{
+  home.file.".config/rofi/catppuccin-mocha.rasi" = {
+    source = ./catppuccin-mocha.rasi;
+  };
+
+  programs.rofi = {
+    enable = true;
+    extraConfig = {
+      modi = "run,drun,window";
+      font = "Noto Sans CJK JP 12";
+      show-icons = true;
+      disable-history = true;
+      hover-select = true;
+      bw = 0;
+      display-drun = "   Apps ";
+      display-run = "   Run ";
+      display-window = "   Window";
+      display-network = "   Network";
+      icon-theme = "Oranchelo";
+      terminal = "alacritty";
+      drun-match-fields = "name";
+      drun-display-format = "{icon} {name}";
+      me-select-entry = "";
+      me-accept-entry = "MousePrimary";
+      kb-cancel = "Escape,MouseMiddle";
+    };
+    theme = "catppuccino-mocha";
+  };
+}
