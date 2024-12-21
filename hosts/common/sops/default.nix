19314c0e63e07e4361b261a9b194bf98702d46d8
Cedrik Hoffmann <dev@choffmann.io>
2024-12-21 14:23:16 +0100

    feat: change secret user key

--- hosts/common/sops/default.nix
+++ hosts/common/sops/default.nix
@@ -17,7 +17,7 @@
     };
 
     secrets = {
-      "user_age_keys/${config.users.users.choffmann.name}_${config.networking.hostName}" = {
+      "user_age_keys/${config.users.users.choffmann.name}" = {
         owner = config.users.users.choffmann.name;
         inherit (config.users.users.choffmann) group;
         path = "/home/choffmann/.config/sops/age/keys.txt";
