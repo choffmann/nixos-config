3881f8515b1fdb6243ba275f75d867ede78f3954
Cedrik Hoffmann <choffmann@progeek.de>
2025-01-17 15:20:13 +0100

    chore: change browser config

--- home/choffmann/common/optional/brower/chromium.nix
+++ home/choffmann/common/optional/brower/chromium.nix
@@ -1,16 +1,24 @@
-{pkgs, ...}:
-{
+{pkgs, ...}: {
   programs.chromium = {
     enable = true;
     extensions = [
       # bitwarden
       "nngceckbapebfimnlniiiahkandclblb"
+
+      # react dev tools
+      "fmkadmapgofadopljbjfkapdkoienihi"
     ];
 
     commandLineArgs = [
       "--ignore-gpu-blocklist"
+      "--no-default-browser-check"
       "--enable-zero-copy"
     ];
   };
+  xdg.mimeApps.defaultApplications = {
+    "text/html" = ["chromium.desktop"];
+    "text/xml" = ["chromium.desktop"];
+    "x-scheme-handler/http" = ["chromium.desktop"];
+    "x-scheme-handler/https" = ["chromium.desktop"];
+  };
 }
-
--- home/choffmann/common/optional/brower/firefox.nix
+++ home/choffmann/common/optional/brower/firefox.nix
@@ -1,12 +1,73 @@
-{...}: {
+{config, ...}: let
+  homeDir = config.home.homeDirectory;
+in {
   programs.firefox = {
     enable = true;
     languagePacks = ["de"];
     policies = {
+      AppAutoUpdate = true;
+      BackgroundAppUpdate = false;
+      DefaultDownloadDirectory = "${config.home.homeDirectory}/downloads";
+      DisableBuiltinPDFViewer = false;
       DisableFirefoxStudies = true;
       DisableTelemetry = true;
+      DisableFirefoxAccounts = false; # Enable Firefox Sync
+      DisablePocket = true;
       DontCheckDefaultBrowser = true;
-      OfferToSaveLogins = false;
+      OfferToSaveLogins = false; # Managed by vaultwarden
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+        EmailTracking = true;
+        # Exceptions = ["https://example.com"]
+      };
+
+      ExtensionUpdate = true;
+
+      ExtensionSettings = let
+        extension = shortId: uuid: {
+          name = uuid;
+          value = {
+            install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+            installation_mode = "normal_installed";
+          };
+        };
+      in
+        builtins.listToAttrs [
+          (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
+          (extension "react-devtools" "@react-devtools")
+        ];
+    };
+    profiles.main = {
+      id = 0;
+      name = "Cedrik";
+      isDefault = true;
+      settings = {
+        "signon.rememberSignons" = false; # Disable built-in password manager
+        "browser.compactmode.show" = true;
+        "browser.uidensity" = 1; # enable compact mode
+        "browser.aboutConfig.showWarning" = false;
+        "browser.download.dir" = "${homeDir}/downloads";
+
+        "browser.tabs.firefox-view" = true; # Sync tabs across devices
+        "extensions.pocket.enabled" = false;
+      };
+
+      userChrome = ''
+        /* Hide tab bar in FF Quantum */
+        @-moz-document url("chrome://browser/content/browser.xul") {
+          #TabsToolbar {
+            visibility: collapse !important;
+            margin-bottom: 21px !important;
+          }
+
+          #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"] #sidebar-header {
+            visibility: collapse !important;
+          }
+        }
+      '';
     };
   };
 }
