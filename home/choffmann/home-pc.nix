21f7f62be60884a46c196f886c24fa9b2fc3e081
Cedrik Hoffmann <dev@choffmann.io>
2025-03-09 16:29:02 +0100

    feat(home-pc): add special workspace in hyprland for windows vm

--- home/choffmann/home-pc.nix
+++ home/choffmann/home-pc.nix
@@ -1,4 +1,18 @@
-{config, ...}: {
+{pkgs, ...}: let
+  virtLookingGlassHandler = let
+    vmName = "win10";
+  in
+    pkgs.writeShellApplication {
+      name = "virt-hyprland-handler";
+      text = ''
+        if [ "$(virsh --connect qemu:///system domstate ${vmName})" != "running" ]; then
+          virsh --connect qemu:///system start ${vmName}
+        fi
+
+        looking-glass-client -F
+      '';
+    };
+in {
   imports = [
     ./common/core
 
@@ -12,4 +26,15 @@
 
   # services.yubikey-touch-detector.enable = true;
   # services.yubikey-touch-detector.notificationSound = true;
+
+  # hyprland overrides
+  wayland.windowManager.hyprland.settings = {
+    workspace = [
+      "special:windows, on-created-empty:${virtLookingGlassHandler}/bin/virt-hyprland-handler"
+    ];
+
+    bind = [
+      "$mod + SHIFT, W, togglespecialworkspace, windows"
+    ];
+  };
 }
