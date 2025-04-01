59cd40039e2e38391b75f11573d2a106117c6b1e
Cedrik Hoffmann <choffmann@progeek.de>
2025-04-01 14:27:43 +0200

    chore: update yubikey module

--- modules/nixos/yubikey/default.nix
+++ modules/nixos/yubikey/default.nix
@@ -1,15 +1,18 @@
-{config, pkgs, lib, ...}:
-let
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
   homeDirectory = "/home/choffmann";
-  yubikey-up =
-    let
-      yubikeyIds = lib.concatStringsSep " " (
-        lib.mapAttrsToList (name: id: "[${name}]=\"${builtins.toString id}\"") config.yubikey.identifiers
-      );
-    in
+  yubikey-up = let
+    yubikeyIds = lib.concatStringsSep " " (
+      lib.mapAttrsToList (name: id: "[${name}]=\"${builtins.toString id}\"") config.yubikey.identifiers
+    );
+  in
     pkgs.writeShellApplication {
       name = "yubikey-up";
-      runtimeInputs = builtins.attrValues { inherit (pkgs) gawk yubikey-manager; };
+      runtimeInputs = builtins.attrValues {inherit (pkgs) gawk yubikey-manager;};
       text = ''
         #!/usr/bin/env bash
         set -euo pipefail
@@ -48,13 +51,12 @@ let
       rm ${homeDirectory}/.ssh/id_yubikey.pub
     '';
   };
-in 
-{
+in {
   options = {
     yubikey = {
       enable = lib.mkEnableOption "Enable yubikey support";
       identifiers = lib.mkOption {
-        default = { };
+        default = {};
         type = lib.types.attrsOf lib.types.int;
         description = "Attrset of Yubikey serial numbers. NOTE: Yubico's 'Security Key' products do not use unique serial number therefore, the scripts in this module are unable to distinguish between multiple 'Security Key' devices and instead will detect a Security Key serial number as the string \"[FIDO]\". This means you can only use a single Security Key but can still mix it with YubiKey 4 and 5 devices.";
         example = lib.literalExample ''
@@ -68,10 +70,11 @@ in
     };
   };
 
-config = lib.mkIf config.yubikey.enable {
+  config = lib.mkIf config.yubikey.enable {
     environment.systemPackages = lib.flatten [
       (builtins.attrValues {
-        inherit (pkgs)
+        inherit
+          (pkgs)
           yubioath-flutter
           yubikey-manager
           pam_u2f # for yubikey with sudo
@@ -125,7 +128,7 @@ config = lib.mkIf config.yubikey.enable {
 
     # Yubikey required services and config. See Dr. Duh NixOS config for reference
     services.pcscd.enable = true; # smartcard service
-    services.udev.packages = [ pkgs.yubikey-personalization ];
+    services.udev.packages = [pkgs.yubikey-personalization];
 
     # FIXME(yubikey): Check if this exists on darwin
     services.yubikey-agent.enable = true;
