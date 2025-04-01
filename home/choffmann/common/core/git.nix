a4b7be43456cfb1a7383ac1212948f8c9cbd93a5
Cedrik Hoffmann <choffmann@progeek.de>
2025-04-01 10:40:30 +0200

    feat: add gpg with yubikey and git signed commits

--- home/choffmann/common/core/git.nix
+++ home/choffmann/common/core/git.nix
@@ -1,9 +1,15 @@
-{lib, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.lazygit.enable = true;
   programs.git = {
     enable = true;
     userEmail = lib.mkDefault "dev@choffmann.io";
     userName = lib.mkDefault "Cedrik Hoffmann";
+    signing.key = "3BC97278FCE8CD8D";
+    extraConfig.commit.gpgsign = true;
     extraConfig = {
       pull.rebase = "true";
       url = {
@@ -13,10 +19,58 @@
         "ssh://git@gitlab.com" = {
           insteadOf = "https://gitlab.com";
         };
+        "ssh://gitea@git.progeek.de:2222" = {
+          insteadOf = "https://git.progeek.de";
+        };
       };
     };
     ignores = [
       ".direnv"
     ];
+  };
+
+  # https://joinemm.dev/blog/yubikey-nixos-guide
+  programs.gpg = {
+    enable = true;
+
+    # https://support.yubico.com/hc/en-us/articles/4819584884124-Resolving-GPG-s-CCID-conflicts
+    scdaemonSettings = {
+      disable-ccid = true;
+    };
+
+    # https://github.com/drduh/config/blob/master/gpg.conf
+    settings = {
+      personal-cipher-preferences = "AES256 AES192 AES";
+      personal-digest-preferences = "SHA512 SHA384 SHA256";
+      personal-compress-preferences = "ZLIB BZIP2 ZIP Uncompressed";
+      default-preference-list = "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
+      cert-digest-algo = "SHA512";
+      s2k-digest-algo = "SHA512";
+      s2k-cipher-algo = "AES256";
+      charset = "utf-8";
+      fixed-list-mode = true;
+      no-comments = true;
+      no-emit-version = true;
+      keyid-format = "0xlong";
+      list-options = "show-uid-validity";
+      verify-options = "show-uid-validity";
+      with-fingerprint = true;
+      require-cross-certification = true;
+      no-symkey-cache = true;
+      use-agent = true;
+      throw-keyids = true;
+    };
+  };
+
+  services.gpg-agent = {
+    enable = true;
+
+    # https://github.com/drduh/config/blob/master/gpg-agent.conf
+    defaultCacheTtl = 60;
+    maxCacheTtl = 120;
+    pinentryPackage = pkgs.pinentry-curses;
+    extraConfig = ''
+      ttyname $GPG_TTY
+    '';
   };
 }
