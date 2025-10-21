4fbb840f67fa4e8d7c591e3b542bc581cb6e7629
Cedrik Hoffmann <dev@choffmann.io>
2025-10-21 22:53:40 +0200

    feat: update android module

--- hosts/common/optional/android.nix
+++ hosts/common/optional/android.nix
@@ -1,6 +1,14 @@
 {pkgs, ...}: {
-  environment.systemPackages = [pkgs.unstable.android-studio pkgs.unstable.cargo-ndk];
+  environment.systemPackages = [pkgs.unstable.android-studio pkgs.unstable.cargo-ndk pkgs.libusb1];
 
   programs.adb.enable = true;
-  users.users.choffmann.extraGroups = ["adbusers"];
+  users.groups.plugdev = {};
+  users.users.choffmann.extraGroups = ["adbusers" "plugdev"];
+
+  services.udev.extraRules = ''
+    # Samsung (Galaxy S21 Ultra)
+    SUBSYSTEM=="usb", ATTR{idVendor}=="04e8", MODE="0666", GROUP="plugdev"
+  '';
+
+  boot.blacklistedKernelModules = ["cdc_acm"];
 }
