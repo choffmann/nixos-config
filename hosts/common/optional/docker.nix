6cf4d047fd692d2a1f1bff269976dd3f6e91d085
Cedrik Hoffmann <dev@choffmann.io>
2026-06-12 10:39:41 +0200

    feat(docker): add docker-buildx

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -1,8 +1,10 @@
-{
+{pkgs, ...}: {
   virtualisation.docker = {
     enable = true;
     enableOnBoot = false;
   };
+
+  environment.systemPackages = [pkgs.docker-buildx];
 
   systemd.sockets.docker.wantedBy = ["sockets.target"];
 }
