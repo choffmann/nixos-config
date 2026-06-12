6af9bac93fd4329fe4832494a17df3a7392683c7
Cedrik Hoffmann <dev@choffmann.io>
2026-06-12 10:39:44 +0200

    feat(ssh): authorize pam_rssh sudo via yubikey pubkey

--- hosts/common/optional/services/openssh.nix
+++ hosts/common/optional/services/openssh.nix
@@ -5,6 +5,7 @@
   ...
 }: let
   sshPort = 22;
+  yubikeyPubKey = lib.custom.relativeToRoot "hosts/common/users/choffmann/keys/id_yubi.pub";
 in {
   services.openssh = {
     enable = true;
@@ -24,11 +25,11 @@ in {
 
   security.pam.services.sudo = {config, ...}: {
     rules.auth.rssh = {
-      order = config.rules.auth.ssh_agent_auth.order - 1;
+      order = 10500;
       control = "sufficient";
       modulePath = "${pkgs.pam_rssh}/lib/libpam_rssh.so";
       settings.authorized_keys_command = pkgs.writeShellScript "get-authorized-keys" ''
-        cat "/etc/ssh/authorized_keys.d/$1"
+        cat "${yubikeyPubKey}"
       '';
     };
   };
