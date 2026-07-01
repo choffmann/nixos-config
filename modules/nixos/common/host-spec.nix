529a27052d01ad3b18f556ca35fa9a1dfa0978a6
Cedrik Hoffmann <dev@choffmann.io>
2026-07-01 18:51:42 +0200

    refactor(host-spec): drop 16 unused options, keep only consumed ones

--- modules/nixos/common/host-spec.nix
+++ modules/nixos/common/host-spec.nix
@@ -15,42 +15,6 @@
       type = lib.types.str;
       description = "The hostname of the host";
     };
-    email = lib.mkOption {
-      type = lib.types.attrsOf lib.types.str;
-      description = "The email of the user";
-    };
-    flake = lib.mkOption {
-      type = lib.types.str;
-      description = "Location of flake";
-      default =
-        let
-          home = config.hostSpec.home;
-        in
-        "${home}/nixos-config";
-    };
-    # FIXME: Set an assert to make sure this is set if isWork is true
-    work = lib.mkOption {
-      default = { };
-      type = lib.types.attrsOf lib.types.anything;
-      description = "An attribute set of work-related information if isWork is true";
-    };
-    networking = lib.mkOption {
-      default = { };
-      type = lib.types.attrsOf lib.types.anything;
-      description = "An attribute set of networking information";
-    };
-    domain = lib.mkOption {
-      type = lib.types.str;
-      description = "The domain of the host";
-    };
-    userFullName = lib.mkOption {
-      type = lib.types.str;
-      description = "The full name of the user";
-    };
-    handle = lib.mkOption {
-      type = lib.types.str;
-      description = "The handle of the user (eg: github user)";
-    };
     home = lib.mkOption {
       type = lib.types.str;
       description = "The home directory of the user";
@@ -60,59 +24,10 @@
         in
         if pkgs.stdenv.isLinux then "/home/${user}" else "/Users/${user}";
     };
-    # FIXME: This should probably just switch to an impermenance option?
-    persistFolder = lib.mkOption {
+    flake = lib.mkOption {
       type = lib.types.str;
-      description = "The folder to persist data if impermenance is enabled";
-      default = "/persist";
-    };
-    isMinimal = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Used to indicate a minimal host";
-    };
-    isProduction = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Used to indicate a production host";
-    };
-    isServer = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Used to indicate a server host";
-    };
-    isWork = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Used to indicate a host that uses work resources";
-    };
-    # Sometimes we can't use pkgs.stdenv.isLinux due to infinite recursion
-    isDarwin = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Used to indicate a host that is darwin";
-    };
-    useYubikey = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Used to indicate if the host uses a yubikey";
-    };
-    voiceCoding = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Used to indicate a host that uses voice coding";
-    };
-
-    # FIXME: Maybe make this display sub options or something later
-    hdr = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Used to indicate a host that uses HDR";
-    };
-    scaling = lib.mkOption {
-      type = lib.types.str;
-      default = "1";
-      description = "Used to indicate what scaling to use. Floating point number";
+      description = "Location of the flake on the host";
+      default = "${config.hostSpec.home}/nixos-config";
     };
   };
 }
