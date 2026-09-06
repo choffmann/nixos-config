f7c853c2e31f57473cf23e028eaf4a13399dea50
Cedrik Hoffmann <dev@choffmann.io>
2026-09-06 15:43:28 +0200

    fix(wayland): drop global QT_QPA_PLATFORM=xcb override

--- hosts/common/optional/wayland.nix
+++ hosts/common/optional/wayland.nix
@@ -1,9 +1,5 @@
 { pkgs, ... }:
 {
-  environment.sessionVariables = {
-    QT_QPA_PLATFORM = "xcb";
-  };
-
   environment.systemPackages = with pkgs; [
     grim # screen capture component, required by flameshot
 
